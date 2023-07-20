209c94f14fdd02c726561a8ff152730e52d5ce36
LaPatusek <ddrrusek@gmail.com>
2023-07-20 16:48:41 +0200

    pierunskie platnosci

--- src/Components/Stripe/PaymentForm.js
+++ src/Components/Stripe/PaymentForm.js
@@ -1,29 +1,51 @@
-import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
+import {
+  AddressElement,
+  CardElement,
+  useElements,
+  useStripe,
+} from '@stripe/react-stripe-js';
 import axios from 'axios';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
+import { Link } from 'react-router-dom';
+import GoBackButton from '../Elements/GoBackButton';
 import styles from './PaymentForm.module.css';
 
+const ADDRESS_OPTIONS = {
+  mode: 'billing',
+  fields: {
+    phone: 'always',
+  },
+  validation: {
+    phone: {
+      required: 'always',
+    },
+  },
+};
+
 const CARD_OPTIONS = {
+  hidePostalCode: true,
+  disableLink: true,
   iconStyle: 'solid',
   style: {
     base: {
-      iconColor: '#c4f0ff',
+      backgroundColor: '#3db60c',
+      iconColor: '#fff',
       color: '#fff',
       fontWeight: 500,
-      fontFamily: 'Roboto, Open Sans, Segoe UI, sans-serif',
       fontSize: '16px',
       fontSmoothing: 'antialiased',
-      ':-webkit-autofill': { color: '#fce883' },
-      '::placeholder': { color: '#87bbfd' },
+      ':-webkit-autofill': { color: '#fff' },
+      '::placeholder': { color: '#fff' },
     },
     invalid: {
-      iconColor: '#ffc7ee',
-      color: '#ffc7ee',
+      iconColor: '#f25555',
+      color: '#f25555',
     },
   },
 };
 
 const PaymentForm = () => {
+  const emailRef = useRef();
   const [success, setSuccess] = useState(false);
   const stripe = useStripe();
   const elements = useElements();
@@ -33,13 +55,28 @@ const PaymentForm = () => {
     const { error, paymentMethod } = await stripe.createPaymentMethod({
       type: 'card',
       card: elements.getElement(CardElement),
+
+      billing_details: {
+        address: {
+          city: 'Niebylec',
+          country: 'PL',
+          line1: 'Jawornik',
+          line2: '328',
+          postal_code: '38-114',
+        },
+        name: 'Dawid',
+        email: emailRef?.current.value,
+        phone: '+48 312 123 312',
+      },
     });
+
+    console.log();
 
     if (!error) {
       try {
         const { id } = paymentMethod;
         const response = await axios.post('http://localhost:4000/payment', {
-          amount: 1000,
+          amount: 45000, //amount to grosze czyli 1000 = 10.00zł
           id,
         });
         if (response.data.success) {
@@ -57,19 +94,27 @@ const PaymentForm = () => {
   return (
     <div>
       {!success ? (
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit} className={styles['payment-form']}>
+          <input ref={emailRef}></input>
           <fieldset className={styles['FormGroup']}>
             <div className='FormRow'>
+              <AddressElement options={ADDRESS_OPTIONS} />
               <CardElement options={CARD_OPTIONS} />
             </div>
           </fieldset>
-          <button>Pay</button>
+          <button>zapłać</button>
         </form>
       ) : (
-        <div>
+        <div className={styles['message-after-payment']}>
           <h2>
-            Dziękuje za zakupienie witryny!
+            Dziękujemy za zakupienie witryny! <br />W ciągu 48h skontaktujemy
+            się z Tobą!
           </h2>
+          <div className={`${styles['back-button']} grid`}>
+            <Link to='/main'>
+              <GoBackButton />
+            </Link>
+          </div>
         </div>
       )}
     </div>
