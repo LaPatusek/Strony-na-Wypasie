d609f8ca78ef33b455996f1ab1f401523008f0a8
LaPatusek <ddrrusek@gmail.com>
2023-07-21 15:52:13 +0200

    pierunskie platnosci

--- src/Components/Stripe/PaymentForm.js
+++ src/Components/Stripe/PaymentForm.js
@@ -1,26 +1,12 @@
-import {
-  AddressElement,
-  CardElement,
-  useElements,
-  useStripe,
-} from '@stripe/react-stripe-js';
+import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
 import axios from 'axios';
+import { ArrowLeft } from 'iconsax-react';
 import { useRef, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
+import cowBronze from '../../Assets/cow-bronze.webp';
 import GoBackButton from '../Elements/GoBackButton';
+import useInput from '../hooks/useInput';
 import styles from './PaymentForm.module.css';
-
-const ADDRESS_OPTIONS = {
-  mode: 'billing',
-  fields: {
-    phone: 'always',
-  },
-  validation: {
-    phone: {
-      required: 'always',
-    },
-  },
-};
 
 const CARD_OPTIONS = {
   hidePostalCode: true,
@@ -32,7 +18,7 @@ const CARD_OPTIONS = {
       iconColor: '#fff',
       color: '#fff',
       fontWeight: 500,
-      fontSize: '16px',
+      fontSize: '22px',
       fontSmoothing: 'antialiased',
       ':-webkit-autofill': { color: '#fff' },
       '::placeholder': { color: '#fff' },
@@ -45,32 +31,106 @@ const CARD_OPTIONS = {
 };
 
 const PaymentForm = () => {
-  const emailRef = useRef();
   const [success, setSuccess] = useState(false);
   const stripe = useStripe();
   const elements = useElements();
+  const emailRef = useRef();
+  const nameRef = useRef();
+  const addressFirstRef = useRef();
+  const addressSecondRef = useRef();
+  const postalCodeRef = useRef();
+  const cityRef = useRef();
+  const telRef = useRef();
+  const history = useHistory();
+
+  const navigationFunction = () => {
+    history.goBack();
+  };
+
+  const {
+    value: enteredName,
+    isValid: nameIsValid,
+    valueChangeHandler: nameChangeHandler,
+    inputBlurHandler: nameBlurHandler,
+    hasError: nameHasError,
+  } = useInput((value) => value.trim() !== '');
+
+  const {
+    value: enteredMail,
+    isValid: mailIsValid,
+    valueChangeHandler: mailChangeHandler,
+    inputBlurHandler: mailBlurHandler,
+    hasError: mailHasError,
+  } = useInput((value) => value.trim().includes('@'));
+
+  const {
+    value: enteredFirstAdd,
+    isValid: firstAddIsValid,
+    valueChangeHandler: firstAddChangeHandler,
+    inputBlurHandler: firstAddBlurHandler,
+    hasError: firstAddHasError,
+  } = useInput((value) => value.trim() !== '');
+
+  const {
+    value: enteredPostal,
+    isValid: postalIsValid,
+    valueChangeHandler: postalChangeHandler,
+    inputBlurHandler: postalBlurHandler,
+    hasError: postalHasError,
+  } = useInput((value) => value.trim() !== '');
+
+  const {
+    value: enteredCity,
+    isValid: cityIsValid,
+    valueChangeHandler: cityChangeHandler,
+    inputBlurHandler: cityBlurHandler,
+    hasError: cityHasError,
+  } = useInput((value) => value.trim() !== '');
+
+  const {
+    value: enteredNumber,
+    isValid: numberIsValid,
+    valueChangeHandler: numberChangeHandler,
+    inputBlurHandler: numberBlurHandler,
+    hasError: numberHasError,
+  } = useInput((value) => value.trim() !== '');
+
+  let formIsValid = false;
+
+  if (
+    nameIsValid &&
+    mailIsValid &&
+    firstAddIsValid &&
+    postalIsValid &&
+    cityIsValid &&
+    numberIsValid
+  ) {
+    formIsValid = true;
+  }
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    if (!formIsValid) {
+      return;
+    }
+
     const { error, paymentMethod } = await stripe.createPaymentMethod({
       type: 'card',
       card: elements.getElement(CardElement),
 
       billing_details: {
         address: {
-          city: 'Niebylec',
+          city: cityRef?.current.value,
           country: 'PL',
-          line1: 'Jawornik',
-          line2: '328',
-          postal_code: '38-114',
+          line1: addressFirstRef?.current.value,
+          line2: addressSecondRef?.current.value,
+          postal_code: postalCodeRef?.current.value,
         },
-        name: 'Dawid',
+        name: nameRef?.current.value,
         email: emailRef?.current.value,
-        phone: '+48 312 123 312',
+        phone: telRef?.current.value,
       },
     });
-
-    console.log();
 
     if (!error) {
       try {
@@ -94,16 +154,153 @@ const PaymentForm = () => {
   return (
     <div>
       {!success ? (
-        <form onSubmit={handleSubmit} className={styles['payment-form']}>
-          <input ref={emailRef}></input>
-          <fieldset className={styles['FormGroup']}>
-            <div className='FormRow'>
-              <AddressElement options={ADDRESS_OPTIONS} />
-              <CardElement options={CARD_OPTIONS} />
+        <div className={`${styles['payment-wrap']} grid`}>
+          <div className={styles['left-section']}>
+            <button onClick={navigationFunction}>
+              <ArrowLeft size='18' color='#000000' /> Powrót
+            </button>
+            <div className={`${styles['product-wrap']} grid`}>
+              <img src={cowBronze} alt='Mała Krówka' />
+              <div className={styles['product-text']}>
+                <h2>Mała krówka</h2>
+                <h3>450 PLN</h3>
+              </div>
             </div>
-          </fieldset>
-          <button>zapłać</button>
-        </form>
+          </div>
+          <div className={styles['right-section']}>
+            <form onSubmit={handleSubmit} className={styles['payment-form']}>
+              <div className={`${styles['upper-form']} grid`}>
+                <div
+                  className={`${styles.group} grid ${
+                    nameHasError ? styles.error : ''
+                  }`}
+                >
+                  <label htmlFor='name'>Imię i nazwisko</label>
+                  <input
+                    type='text'
+                    ref={nameRef}
+                    value={enteredName}
+                    onChange={nameChangeHandler}
+                    onBlur={nameBlurHandler}
+                    id='name'
+                    required='Imie i nazwisko'
+                    placeholder='Imię i nazwisko'
+                    autoComplete='false'
+                  />
+                </div>
+
+                <div
+                  className={`${styles.group} grid ${
+                    mailHasError ? styles.error : ''
+                  }`}
+                >
+                  <label htmlFor='mail'>Email</label>
+                  <input
+                    type='email'
+                    ref={emailRef}
+                    value={enteredMail}
+                    onChange={mailChangeHandler}
+                    onBlur={mailBlurHandler}
+                    id='mail'
+                    required='mail'
+                    placeholder='Adres email'
+                    autoComplete='false'
+                  />
+                </div>
+
+                <div
+                  className={`${styles.group} grid ${
+                    firstAddHasError ? styles.error : ''
+                  }`}
+                >
+                  <label htmlFor='addressFirst'>Wiersz adresu 1</label>
+                  <input
+                    type='text'
+                    ref={addressFirstRef}
+                    value={enteredFirstAdd}
+                    onChange={firstAddChangeHandler}
+                    onBlur={firstAddBlurHandler}
+                    id='addressFirst'
+                    placeholder='Ulica'
+                    required='Wiersz adresu'
+                  />
+                </div>
+
+                <div className={`${styles.group} grid`}>
+                  <label htmlFor='addressSecond'>Wiersz adresu 2*</label>
+                  <input
+                    type='text'
+                    ref={addressSecondRef}
+                    id='addressSecond'
+                    placeholder='Mieszkanie, numer budynku, itp. (opcjonalnie)'
+                  />
+                </div>
+
+                <div
+                  className={`${styles.group} grid ${
+                    postalHasError ? styles.error : ''
+                  }`}
+                >
+                  <label htmlFor='postalCode'>Kod pocztowy</label>
+                  <input
+                    type='text'
+                    ref={postalCodeRef}
+                    value={enteredPostal}
+                    onChange={postalChangeHandler}
+                    onBlur={postalBlurHandler}
+                    id='postalCode'
+                    required='Kod pocztowy'
+                    placeholder='00-000'
+                  />
+                </div>
+
+                <div
+                  className={`${styles.group} grid ${
+                    cityHasError ? styles.error : ''
+                  }`}
+                >
+                  <label htmlFor='city'>Miasto</label>
+                  <input
+                    type='text'
+                    ref={cityRef}
+                    id='city'
+                    required='Miasto'
+                    value={enteredCity}
+                    onChange={cityChangeHandler}
+                    onBlur={cityBlurHandler}
+                    placeholder='Miasto'
+                  />
+                </div>
+
+                <div
+                  className={`${styles.group} grid ${
+                    numberHasError ? styles.error : ''
+                  }`}
+                >
+                  <label htmlFor='phone'>Numer telefonu</label>
+                  <input
+                    type='number'
+                    ref={telRef}
+                    value={enteredNumber}
+                    onChange={numberChangeHandler}
+                    onBlur={numberBlurHandler}
+                    id='phone'
+                    required='Telefon'
+                    autoComplete='false'
+                    placeholder='+48 000 000 000'
+                  />
+                </div>
+              </div>
+
+              <fieldset className={styles.fieldset}>
+                <div className={styles['FormGroup']}>
+                  <CardElement options={CARD_OPTIONS} />
+                </div>
+              </fieldset>
+              <button>zapłać</button>
+            </form>
+          </div>
+        </div>
       ) : (
         <div className={styles['message-after-payment']}>
           <h2>
