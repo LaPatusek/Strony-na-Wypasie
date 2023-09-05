eaec7498e8f731fc97ccbbc547870d7713b1bcb6
LaPatusek <ddrrusek@gmail.com>
2023-09-05 11:36:24 +0200

    404 page

--- src/Pages/PageNotFound.js
+++ src/Pages/PageNotFound.js
@@ -1,11 +1,39 @@
-import { Link } from 'react-router-dom';
+import { ArrowLeft2, ArrowRight2 } from 'iconsax-react';
+import { Link, useHistory } from 'react-router-dom';
+import logo from '../Assets/logo3.webp';
+import styles from './PageNotFound.module.css';
 
 const PageNotFound = () => {
+  const history = useHistory();
+
+  const navigationFunction = () => {
+    history.goBack();
+  };
+
   return (
-    <div>
-      <h1>Strony na wypasie</h1>
-      <h2>Strony nie znaleziono</h2>
-      <Link></Link>
+    <div className={`${styles.wrap} grid`}>
+      <div className={`${styles['page-not-found']} grid`}>
+        <img src={logo} alt='Strony Internetowe' />
+        <div className={styles['text-section']}>
+          <h1>Strony na wypasie</h1>
+          <h2>
+            Strony nie znaleziono (
+            <span className={styles['error-code']}>404</span>)
+          </h2>
+          <div className={styles.buttons}>
+            <div className={styles.link}>
+              <button onClick={navigationFunction}>
+                <ArrowLeft2 size='20px' /> Powrót
+              </button>
+            </div>
+            <div className={styles.link}>
+              <Link to='/main'>
+                Strona główna <ArrowRight2 size='20px' />
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
